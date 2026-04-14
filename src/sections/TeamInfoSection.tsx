61fa00db3887e4ab370ebc0a9df5c6b5293af002
David Farfán Castro <davidfarfancastro@192.168.1.120>
2026-04-14 10:52:31 -0600

    GREAT PROJECT3

--- src/sections/TeamInfoSection.tsx
+++ src/sections/TeamInfoSection.tsx
@@ -1,6 +1,6 @@
 import { motion } from 'framer-motion';
 
-const members = ['Kamila', 'Enrique', 'Jorge', 'Jesus'];
+const members = ['Kamila', 'Enrique', 'Jorge', 'Jesus', 'David', 'Iván'];
 
 const TeamInfoSection = () => (
   <section className="relative w-full overflow-hidden py-10 px-[5vw]">
